e07a56f8141c6d49e24957ec7a6ad8b1e763e17d
Campbell Allen <campbell.allen@gmail.com>
2014-10-25 10:52:46 +0100

    towards json formatted message with data headers

--- stream_server.js
+++ stream_server.js
@@ -12,21 +12,33 @@ if ( process.argv.length < 2 ) {
 
 
 //var file_path = 'test/test_json_stream_data.json'
-var file_path = 'test/test_data.csv';
-var file_line_stream = byline(fs.createReadStream(file_path, { encoding: 'utf8' }));
+//var file_path = 'test/test_data.csv';
+var file_path = 'data/zoo2MainSpecz.csv';
+var file_line_stream = byline(fs.createReadStream(file_path, { encoding: 'utf8' })),
+    counter = 0,
+		headers = "";
 
 var WEBSOCKET_PORT = process.argv[4] || 8084,
+    WEBSOCKET_HEADER_PORT = WEBSOCKET_PORT + 1,
     width = 320,
 	  height = 240;
 
-// Websocket Server
+// Websocket Data Server
 var socketServer = new(require('ws').Server)({port: WEBSOCKET_PORT});
 socketServer.on('connection', function(socket) {
 	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
 
 	file_line_stream.on('data', function(file_line) {
-		console.log("line:" + file_line);
-		setTimeout(function () { send_data(file_line) }, 10000)
+		if (counter == 0) {
+			counter +=1
+			headers = file_line;
+			console.log(headers);
+		}
+		else {
+			counter +=1
+			//console.log("line:" + file_line);
+			setTimeout(function () { send_data(file_line) }, counter * 1000)
+		}
 	});
 	socket.on('close', function(code, message){
 		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
@@ -45,6 +57,10 @@ socketServer.broadcast = function(data, opts) {
 };
 
 function send_data(message) {
+	//TODO: format the data as a json string with '{ 'header' : 'data' } '
+	//message_to_array_of_values
+	//headers to array of values
+	
   socketServer.broadcast(message);
 }
 
